cf0d8c3d9e8f02cade67a203f11b325a5f47a516
Peter Malinovskiy <petermalinovskiy@gmail.com>
2020-05-05 23:46:52 +0300

    Revert "fix: fixed       <div class=container>"
    This reverts commit e73932a85f65ec273d6bab6ef307debce030b3da.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require ('gulp');
-var browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 const minCSS = require ('gulp-clean-css');
 var rename = require("gulp-rename");
 
@@ -12,20 +12,18 @@ gulp.task('hello', function(done) {
 // Static server
 gulp.task('browser-sync', function() {
   browserSync.init({
-      server: {
-          baseDir: "./src"
-      }
+      proxy: "yourlocal.dev"
   });
-  gulp.watch("./src/*.html").on('change', browserSync.reload);
+  gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 gulp.task('min-CSS', function(done){
-  gulp.src('./src/css/*.css')
+  gulp.src('./css/*.css')
     .pipe(minCSS())
     .pipe(rename({
         suffix: '.min'
       }))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./minicss'));
     done( );
 });
 
