28648e4baa00bc7175d5f7d01d2d0c5200cdc93b
Peter Malinovskiy <petermalinovskiy@gmail.com>
2020-05-07 06:46:56 +0300

    feat: add serveSass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,6 @@ const {src, dest, watch} = require ('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require ('gulp-sass');
 
-
-
-
 // Static server
 function bs() {
   serveSass();
@@ -13,18 +10,17 @@ function bs() {
           baseDir: "./src"
       }
   });
-  watch("./src/*.html").on('change', browserSync.reload);
+  watch("./*.html").on('change', browserSync.reload);
   watch("./sass/**/*.sass", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
-
+// Compile sass into CSS & auto-inject into browsers
 function serveSass() {
-  return src("./sass/*.sass")
+  return src("./sass/**/*.sass")
       .pipe(sass())
       .pipe(dest("./css"))
       .pipe(browserSync.stream());
 };
 
 exports.serve = bs;
-
