a789276e11dbdba6d6285a943c712082cfadf731
Peter Malinovskiy <petermalinovskiy@gmail.com>
2020-05-16 15:52:50 +0300

    refact: rewrite js code for modal in jQuery

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -19,13 +20,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 }
 
-
 //закрытие по кнопке Escape
 window.addEventListener('keydown', function (event) {
   if (event.key === 'Escape') {
       modal.classList.remove('modal--visible');
   }
 })
-
   
 })
+*/
+$( document ).ready(function() {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible')
+  })
+
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible')
+  })
+
+});
