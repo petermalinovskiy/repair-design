f16ee943b175ad2d72d71bc242db6bb7d100d4ed
Peter Malinovskiy <petermalinovskiy@gmail.com>
2020-05-17 20:53:07 +0300

    feat: add _projects.sass

--- src/js/main.js
+++ src/js/main.js
@@ -42,4 +42,23 @@ $( document ).ready(function() {
     modal.toggleClass('modal--visible')
   })
 
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 30 + bullets.width() + 30);
+  bullets.css('left', prev.width() + 30)
+
 });
